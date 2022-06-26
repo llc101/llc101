b70a7c34ebd214bf005a50022956c1d277a98856
Yakubu <jnrkhld@gmail.com>
2022-06-26 10:02:28 +0000

    Adding Adverts

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,15 +1,7 @@
 import { useEffect, useRef } from "react";
 
-const Banner = () => {
+const Banner = ({ atOptions }) => {
   const banner = useRef();
-
-  const atOptions = {
-    key: "82a08b05a02857034dd70940d6e862a0",
-    format: "iframe",
-    height: 60,
-    width: 468,
-    params: {},
-  };
 
   useEffect(() => {
     if (!banner.current.firstChild) {
