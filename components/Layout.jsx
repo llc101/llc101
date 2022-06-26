95879cc66029c114c3b06e76a4d1473185883c5e
Yakubu <jnrkhld@gmail.com>
2022-06-26 13:41:20 +0000

    Adding Adverts

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,18 @@
+import Head from "next/head";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 function Layout({ children }) {
   return (
     <div>
+      <Head>
+        <title>Creativity | 101</title>
+        <meta
+          name="description"
+          content="Learn web development, 2d & 3d graphic design and Photo & Video Editing."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
 
       <div className="min-h-[92vh]">{children}</div>
