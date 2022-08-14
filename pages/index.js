47cc13aef9096c70e341748196f7ef125fa15d3d
Yakubu <jnrkhld@gmail.com>
2022-08-14 08:47:43 +0000

    Removed Ads

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import Script from "next/script";
-import Banner from "../components/Banner";
-// import Challenge from "../public/images/pexels-pixabay-163064.jpg";
 
 export default function Home() {
   return (
@@ -51,18 +48,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="hidden md:flex justify-center items-center my-8 px-2">
-          <Banner
-            atOptions={{
-              key: "e4e2ea2bf2913225da167d684b786412",
-              format: "iframe",
-              height: 90,
-              width: 728,
-              params: {},
-            }}
-          />
         </div>
 
         {/* Guides */}
@@ -335,16 +320,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-
-        <div className="p-2">
-          <div id="container-9066d06eeb66c78f46d7292c1ec93e17"></div>
-        </div>
       </main>
-      <Script
-        async="async"
-        data-cfasync="false"
-        src="//pl17405962.profitablecpmgate.com/9066d06eeb66c78f46d7292c1ec93e17/invoke.js"
-      ></Script>
     </div>
   );
 }
